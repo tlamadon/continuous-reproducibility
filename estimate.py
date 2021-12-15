f96dfac5d42ae07baf580717b5ede222613b56ad
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2021-12-14 21:39:49 -0600

    added std to results

--- estimate.py
+++ estimate.py
@@ -2,8 +2,7 @@ import json
 import pandas as pd
 
 df = pd.read_csv('data_clean.csv')
-m = df.Y.mean()
-data = {'mean':m}
+data = {'mean':df.Y.mean(), 'sd': df.Y.std()}
 
 with open('res.txt', 'w') as outfile:
     json.dump(data, outfile)
